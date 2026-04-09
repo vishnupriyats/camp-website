f2a86b6e67bc6fbc808d7f6a67ed67a25acfb7d4
vishnupriyats <vishnupriya0913@gmail.com>
2026-04-09 11:51:09 +0100

    fix: update all Mongoose callbacks to async/await, fix Passport 0.7 compatibility, fix method-override middleware order, add image fallback

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,4 @@
-var Campground = require("../models/campground");
+/* var Campground = require("../models/campground");
 var Comment    = require("../models/comment");
 var middlewareObj = {};
 
@@ -70,5 +70,67 @@ middlewareObj.isLoggedIn = function(req,res,next){
 	res.redirect("/login");
 }
 
+
+module.exports = middlewareObj; */
+
+var Campground = require("../models/campground");
+var Comment    = require("../models/comment");
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = async function(req, res, next) {
+    if(req.isAuthenticated()) {
+        try {
+            var foundCampground = await Campground.findById(req.params.id);
+            if(!foundCampground) {
+                req.flash("error", "Campground not found");
+                return res.redirect("back");
+            }
+            if(foundCampground.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have permission");
+                res.redirect("back");
+            }
+        } catch(err) {
+            req.flash("error", "Campground not found");
+            res.redirect("back");
+        }
+    } else {
+        req.flash("error", "You need to be logged in");
+        res.redirect("back");
+    }
+}
+
+middlewareObj.checkCommentOwnership = async function(req, res, next) {
+    if(req.isAuthenticated()) {
+        try {
+            var foundComment = await Comment.findById(req.params.comment_id);
+            if(!foundComment) {
+                req.flash("error", "Comment not found");
+                return res.redirect("back");
+            }
+            if(foundComment.author.id.equals(req.user._id)) {
+                next();
+            } else {
+                req.flash("error", "You don't have permission");
+                res.redirect("back");
+            }
+        } catch(err) {
+            req.flash("error", "Comment not found");
+            res.redirect("back");
+        }
+    } else {
+        req.flash("error", "You need to be logged in");
+        res.redirect("/login");
+    }
+}
+
+middlewareObj.isLoggedIn = function(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    }
+    req.flash("error", "You need to be logged in");
+    res.redirect("/login");
+}
 
 module.exports = middlewareObj;
